a3f72da18193d14e55183959c1417d1269d3308b
Jeff Lapp <lappjeff20@gmail.com>
2019-10-04 12:36:55 -0400

    Completed ex2 challenge

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,15 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
+    for index in range(length):
+        #Start with None value
+        if index == 0:
+            route[index] = hash_table_retrieve(hashtable, "NONE")
+        #grab value at index - 1 and retreive from hashtable with it
+        #will return the value linked to the previous indexs value in the hashtable
+        else:
+            route[index] = hash_table_retrieve(hashtable, route[index - 1])
     return route
